b8d34d9933005b89e6266e79cbea03455269140b
ethyi <ethyi@proton.me>
2022-09-13 17:30:41 -0700

    basic commits

---
+++ components/layout.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="h-screen flex flex-col">
+      <Head>
+        <title>Paperpetual</title>
+        <meta name="description" content="Trade perpetuals without the risk!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className="text-center p-20">
+        <h1 className="text-7xl font-bold ">Paperpetual</h1>
+      </header>
+      <main className="flex-1">{children}</main>
+      <footer className="text-center p-2 bg-gray-200">
+        <a href="https://ethyi.de/" target="_blank" rel="noopener noreferrer">
+          Made by ethyi &copy; {new Date().getFullYear()}
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default Layout;
--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/ftx/:path*",
+        destination: "https://ftx.com/api/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
